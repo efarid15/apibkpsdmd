d4ddf6afb161680518fe82642bca5308ed6d8e4f
enalfarid <farid.enal@gmail.com>
2019-10-15 22:31:12 +0800

    add git ignore

--- controller/upload.js
+++ controller/upload.js
@@ -3,21 +3,22 @@ const response = require('../res');
 const connection = require('../conn');
 const path = require('path');
 
-const storage = multer.diskStorage({
-    destination : './uploads',
-    filename: function(req, file, cb){
-        cb(null, file.fieldname + '-' + Date.now() +
-        path.extname(file.originalname));
-    }
-});
-
-//init upload
-const upload = multer({
-    storage : storage
-}).single('dokumen');
 
 exports.FileUpload = function (req, res) {
 
+    const storage = multer.diskStorage({
+        destination : './uploads',
+        filename: function(req, file, cb){
+            cb(null, file.fieldname + '-' + Date.now() +
+            path.extname(file.originalname));
+        }
+    });
+    
+    //init upload
+    const upload = multer({
+        storage : storage
+    }).single('dokumen');
+    
     //set storage engine
 
 upload(req, res, err => {
